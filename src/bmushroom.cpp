d89d1772ef7b19fdebf35249b98c4556380367c9
Alexander Kitaev <alex.kitaev58@gmail.com>
2014-03-23 22:00:00 -0400

    Began work on interpreter

--- src/bmushroom.cpp
+++ src/bmushroom.cpp
@@ -14,15 +14,45 @@
 
 using namespace std;
 
+enum Direction
+{
+	RIGHT, LEFT, UP, DOWN
+};
+
 int main(int argc, char** argv)
 {
 	if(argc < 2)
 	{
-		cout << "Usage: bmushroom program.bf" << endl;
+		cout << "Usage: bmushroom [options] program.bf" << endl;
+		cout << "Options:" << endl;
+		cout << "  -a, immediately abort if an erroneous instruction is given"
+			<< endl;
+		cout << "  -w: if an erroneous instruction is given, print a warning "
+			<< "but do not shut down the program" << endl;
+		cout << "  (note: default action on erroneous instructions "
+			<< "is to do nothing)" << endl;
+		cout << "  (note: -a overrides -w if both are set)" << endl;
 		exit(0);
 	}
 
-	string filename = argv[1];
+	bool abortOn = false;
+	bool warningsOn = false;
+
+	string arg;
+	for(int i = 1; i < argc; i++) // search all except for first and last
+	{
+		arg = argv[i];
+		if(arg.find("-a") != string::npos && !abortOn)
+		{
+			abortOn = true;
+		}
+		if(arg.find("-w") != string::npos && !warningsOn)
+		{
+			warningsOn = true;
+		}
+	}
+
+	string filename = argv[argc]; // the last of the arguments
 	ifstream programFile(filename.c_str());
 	vector<string> lines = vector<string>();
 	string temp;
@@ -56,6 +86,106 @@ int main(int argc, char** argv)
 		for(int j = 0; j < line.size(); j++)
 		{
 			program[i][j] = line.at(j);
+		}
+	}
+
+	// execute the program
+	int xPos = 0;
+	int yPos = 0;
+	int a = 0; // temporary variable 1
+	int b = 0; // temporary variable 2
+	bool isStringMode = false;
+	bool skipNextCell = false;
+	bool endOfProgram = false;
+	Direction currDirection = RIGHT;
+	stack<short> programStack = stack<short>(); 
+
+	while(!endOfProgram)
+	{
+		currChar = program[xPos][yPos];
+		if(isStringMode)
+		{
+			if(currChar == '"') isStringMode = false;
+			else programStack.push(currChar);
+		}
+		else if(skipNextCell)
+		{
+			skipNextCell = false; // take no action other than this
+		}
+		else
+		{
+			switch(currChar)
+			{
+				case '0':
+					programStack.push(0);
+					break;
+				case '1':
+					programStack.push(1);
+					break;
+				case '2':
+					programStack.push(2);
+					break;
+				case '3':
+					programStack.push(3);
+					break;
+				case '4':
+					programStack.push(4);
+					break;
+				case '5':
+					programStack.push(5);
+					break;
+				case '6':
+					programStack.push(6);
+					break;
+				case '7':
+					programStack.push(7);
+					break;
+				case '8':
+					programStack.push(8);
+					break;
+				case '9':
+					programStack.push(9);
+					break;
+				case '+':
+					a = programStack.pop();
+					b = programStack.pop();
+					programStack.push(a + b);
+					break;
+				case '-':
+					a = programStack.pop();
+					b = programStack.pop();
+					programStack.push(b - a);
+					break;
+				case '*':
+					a = programStack.pop();
+					b = programStack.pop();
+					programStack.push(a * b);
+					break;
+				case '/':
+					a = programStack.pop();
+					b = programStack.pop();
+					if(a == 0)
+					{
+						if(abortOn)
+						{
+							cerr << "Error: " << filename << " @ " << xPos
+								<< ", " << yPos << ":" << endl;
+							cerr << "Attempted division by 0" << endl;
+							exit(1);
+						}
+						else if(warningsOn)
+						{
+							cerr << "Warning: " << filename << " @ " << xPos
+								<< ", " << yPos << ":" << endl;
+							cerr << "Attempted division by 0" << endl;
+						}
+						// this code gets executed no matter what warningsOn is
+						// because if abortOn is off, 0 is pushed
+						programstack.push(0);
+					}
+					programStack.push(b / a);
+					break;
+			}
 		}
 	}
 }
